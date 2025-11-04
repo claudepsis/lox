7f67c283f5148528f8a89bb4e013f20fd6e4c92e
unknown <claudepsis20@gmail.com>
2025-11-04 17:57:40 +0800

    完成了AST代码编写

---
+++ lox/src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,61 @@
+package com.craftinginterpreters.tool;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+
+/*
+ * .tool instead of .lox this scripts isn't part of the interpreter itself 
+ */
+public class GenerateAst {
+    public static void main(String[] args) throws  IOException{
+        if(args.length!=1){
+            System.err.println("Usage:genrate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir=args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+        "Binary   : Expr left,Token operator,Expr right",
+        "Grouping : Expr expression",
+        "Literal  : Object value",
+        "Unary    : Token operator,Expr right"));
+    }  
+
+    public static void defineAst(String ouputDir,String baseName,List<String> types) throws IOException{
+        String path=ouputDir +"/"+baseName+".java";
+        try (PrintWriter writer = new PrintWriter(path,"UTF-8")) {
+            writer.println("package com.craftinginterpreters.lox;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class "+baseName+"{");
+            for(String type:types){
+                //trim语法用于去除空白字符
+                String className=type.split(":")[0].trim();
+                String filed=type.split(":")[1].trim();
+                defineType(writer, baseName, className, filed);
+            }
+            writer.println("}");
+        }
+    }
+    private static void defineType(PrintWriter writer,String baseName,String className,String fieldList){
+        writer.println("\tstatic class "+className+" extends "+baseName+" {");
+        writer.println("\t\t"+className+"("+fieldList+") {");
+        String[] fields=fieldList.split(",");
+        for (String field :fields){
+            String name=field.split(" ")[1];
+            writer.println("\t\t\tthis."+name+" = "+name+";");
+        }
+        writer.println("\t\t}");
+        writer.println("");
+        for(String field:fields){
+            String typename=field.split(" ")[0];
+            String name=field.split(" ")[1];
+            writer.println("\t\tfinal "+typename+" "+name+";");
+        }
+        writer.println("\t}");
+        writer.println("");
+    }
+
+}
